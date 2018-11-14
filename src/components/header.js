d036fc4810d45faeb9eb11fe52770a4d3e2554b3
Ste <s.o'neill@brassagency.com>
2018-11-14 14:08:24 +0000

    added support for todays date

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,49 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { Component } from 'react'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+import styled from 'styled-components'
 
-export default Header
+let HeaderOuter = styled.header`
+  width: 100%;
+  height: 80vh;
+`
+
+let HeaderInner = styled.div`
+  max-width: 1000px;
+  width: 100%;
+  margin: auto;
+`
+
+let TodaysDate = styled.h2`
+  font-style: serif;
+`
+
+let d = new Date()
+let weekday = new Array(7)
+weekday[0] = 'Sunday'
+weekday[1] = 'Monday'
+weekday[2] = 'Tuesday'
+weekday[3] = 'Wednesday'
+weekday[4] = 'Thursday'
+weekday[5] = 'Friday'
+weekday[6] = 'Saturday'
+
+let today = weekday[d.getDay()]
+
+console.log(today)
+
+export default class header extends Component {
+  render() {
+    return (
+      <HeaderOuter>
+        <HeaderInner>
+          {today !== 'Friday' ? (
+            <TodaysDate>Happy</TodaysDate>
+          ) : (
+            <TodaysDate>Thank god it's</TodaysDate>
+          )}
+          <TodaysDate>{today}</TodaysDate>
+        </HeaderInner>
+      </HeaderOuter>
+    )
+  }
+}
