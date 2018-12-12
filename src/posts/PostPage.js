9f23078e134134de05450c227f489d8b4fea2a70
Ste <s.o'neill@brassagency.com>
2018-12-12 08:57:41 +0000

    Added blog creation support

---
+++ src/posts/PostPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PostPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a post page</h1>
+      </div>
+    )
+  }
+}
