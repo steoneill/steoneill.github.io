95b963f7234802afbd2606276d3853eac1feb445
Ste <ste.tpe@gmail.com>
2019-01-02 16:17:56 +0000

    putting landing button into a new component

--- src/components/LandingHeader.js
+++ src/components/LandingHeader.js
@@ -88,13 +88,19 @@ let HeaderCopy = styled.div`
 `
 
 let CTA = styled(Link)`
-  background: ${props => props.theme.primary};
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 12px;
+  color: #ffffff;
+  letter-spacing: 2px;
+  text-align: center;
   color: white;
   padding: 10px 20px;
-  border-radius: 5px;
-  box-shadow: ${props => props.theme.bsPink};
+  border-radius: 40px;
   text-decoration: none;
   transition: all 0.2s;
+  background-image: linear-gradient(-90deg, #ff9a8b 0%, #ff6a88 100%);
+  box-shadow: 0 7px 14px -7px #ff6d88;
   align-self: self-start;
 
   margin-top: 20px;
@@ -154,7 +160,7 @@ export default class LandingHeader extends Component {
                       data.contentfulHeader.headerCopy.childMarkdownRemark.html,
                   }}
                 />
-                {/* <CTA to={'/contact'}>Get in touch</CTA> */}
+                <CTA to={'/contact'}>Get in touch</CTA>
               </HeaderLeft>
               <HeaderRight>
                 <img src={HeaderImage} />
